b44b815323bed634b7201dc73b3176b04456c4cf
redstoneruin <ryansteinwert@gmail.com>
2019-07-30 20:10:15 -0700

    Sign up form validation

--- untitled-social/src/components/auth/SignUp.js
+++ untitled-social/src/components/auth/SignUp.js
@@ -17,19 +17,113 @@ class SignUp extends Component {
             email: "",
             password: "",
             confirmPassword: "",
-            displayName: ""
+            username: "",
+            submitted: false,
+            validated: false,
+            valid: {
+                email: false,
+                password: false,
+                confirmPassword: false,
+                username: false,
+                emailMessage: null,
+                passwordMessage: null,
+                confirmPasswordMessage: null,
+                usernameMessage: null
+            }
         }
     }
 
+
     /**
-     * Validates input fields, signs up new user if fields correct
+     * validates fields in state
+     */
+    validateState = () => {
+        
+        var valid = {}
+
+        // Validate username
+        if(!this.state.username) {
+            valid.username = false;
+            valid.usernameMessage = "Please enter a username";
+        } else if(this.state.username.length < 5) {
+            valid.username = false;
+            valid.usernameMessage = "Username must be more than 5 characters";
+        } else {
+            valid.username = true;
+            valid.usernameMessage = null;
+        }
+
+        // Validate email
+        if(!this.state.email) {
+            valid.email = false;
+            valid.emailMessage = "Please enter an email";
+        } 
+        // test for email format
+        else if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email)) {
+            valid.email = true;
+            valid.emailMessage = null;
+        } else {
+            valid.email = false;
+            valid.emailMessage = "Please enter a valid email"
+        }
+
+        // validate password
+        if(!this.state.password) {
+            valid.password = false;
+            valid.passwordMessage = "Please enter a password";
+        } else if(this.state.password.length < 8) {
+            valid.password = false;
+            valid.passwordMessage = "Password must be 8 characters or more";
+        } else {
+            valid.password = true;
+            valid.passwordMessage = null;
+        }
+
+        // validate comfirmPassword
+        if(!this.state.confirmPassword) {
+            valid.confirmPassword = false;
+            valid.confirmPasswordMessage = "Please confirm your password";
+        } else if(this.state.confirmPassword !== this.state.password) {
+            valid.confirmPassword = false;
+            valid.confirmPasswordMessage = "Your passwords don't match";
+        } else {
+            valid.confirmPassword = true;
+            valid.confirmPasswordMessage = null;
+        }
+
+        this.setState({
+            valid
+        })
+    }
+
+    /**
+     * Signs up new user if form is valid fields correct
      */
     handleSubmit = (e) => {
         e.preventDefault();
 
+        this.validateState();
+
+        this.setState({
+            submitted: true
+        })
+    }
+
+    /**
+     * Changes the state of this component as form is altered
+     */
+    handleChange = (e) => {
+        // Set state to updated form value
+        this.setState({
+            [e.target.id]: e.target.value
+        }, () => {
+            // Validate form after function callback
+            this.validateState();
+        });
     }
 
     render() {
+
         return (
             <Container className="pt-4">
                 <Row className="justify-content-center">
@@ -37,23 +131,59 @@ class SignUp extends Component {
                             <Card className="shadow-sm">
                                 <Card.Header as="h5">Sign Up</Card.Header>
                                 <Card.Body className="text-left">
-                                    <Form onSubmit={this.handleSubmit}>
-                                        <Form.Group controlId="signupUsername">
+                                    <Form onSubmit={this.handleSubmit} noValidate>
+                                        <Form.Group>
                                             <Form.Label>Username</Form.Label>
-                                            <Form.Control type="text"></Form.Control>
+                                            <Form.Control
+                                                required
+                                                onChange={this.handleChange} 
+                                                type="text" 
+                                                id="username"
+                                                isValid={this.state.valid.username}
+                                                isInvalid={this.state.submitted && !this.state.valid.username}>
+                                                </Form.Control>
+                                                <Form.Control.Feedback type="invalid">{this.state.valid.usernameMessage}</Form.Control.Feedback>
                                         </Form.Group>
-                                        <Form.Group controlId="signupEmail">
+
+                                        <Form.Group>
                                             <Form.Label>Email Address</Form.Label>
-                                            <Form.Control type="email"></Form.Control>
+                                            <Form.Control 
+                                                required
+                                                onChange={this.handleChange} 
+                                                type="email" 
+                                                id="email"
+                                                isValid={this.state.valid.email}
+                                                isInvalid={this.state.submitted && !this.state.valid.email}>
+                                                </Form.Control>
+                                                <Form.Control.Feedback type="invalid">{this.state.valid.emailMessage}</Form.Control.Feedback>
                                         </Form.Group>
-                                        <Form.Group controlId="signupPass">
+
+                                        <Form.Group>
                                             <Form.Label>Password</Form.Label>
-                                            <Form.Control type="password"></Form.Control>
+                                            <Form.Control 
+                                                required
+                                                onChange={this.handleChange} 
+                                                type="password" 
+                                                id="password"
+                                                isValid={this.state.valid.password}
+                                                isInvalid={this.state.submitted && !this.state.valid.password}>
+                                                </Form.Control>
+                                                <Form.Control.Feedback type="invalid">{this.state.valid.passwordMessage}</Form.Control.Feedback>
                                         </Form.Group>
-                                        <Form.Group controlId="signupConfirmPass">
+
+                                        <Form.Group>
                                             <Form.Label>Confirm Password</Form.Label>
-                                            <Form.Control type="password"></Form.Control>
+                                            <Form.Control 
+                                                required
+                                                onChange={this.handleChange} 
+                                                type="password" 
+                                                id="confirmPassword"
+                                                isValid={this.state.valid.confirmPassword}
+                                                isInvalid={this.state.submitted && !this.state.valid.confirmPassword}>
+                                                </Form.Control>
+                                                <Form.Control.Feedback type="invalid">{this.state.valid.confirmPasswordMessage}</Form.Control.Feedback>
                                         </Form.Group>
+
                                         <Button onClick={this.handleSubmit} type="submit" className="primary-button">Sign Up</Button>
                                     </Form>
                                 </Card.Body>
