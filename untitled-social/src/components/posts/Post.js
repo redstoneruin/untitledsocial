ef87fc759a2d20987d62c53c23506443a3bbf0b1
redstoneruin <ryansteinwert@gmail.com>
2019-09-04 19:15:32 -0700

    Image embedding updates

--- untitled-social/src/components/posts/Post.js
+++ untitled-social/src/components/posts/Post.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Container, Row, Col, Card, Spinner} from 'react-bootstrap';
 
-import {getPostByID} from '../../store/actions/postActions';
+import {getPostByID, getSingleFileURLFromPostId} from '../../store/actions/postActions';
+import {getUsernameFromUid} from '../../store/actions/authActions';
 
 import '../../styles/ColorScheme.css';
 
@@ -32,22 +33,52 @@ class Post extends Component {
                 this.setState({
                     post
                 });
-                console.log(post);
+
+                // get username from uid
+                this.props.getUsernameFromUid(post.author)
+                    .then(username => {
+                        this.setState({
+                            username
+                        });
+                    });
+
+                // get and reformat date                    
+                var date = new Date(post.time.seconds * 1000);
+                var dateString = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+                this.setState({
+                    dateString
+                });
+
+                // Get single image url
+                if(post.type === "image") {
+                    this.props.getSingleFileURLFromPostId(post.id)
+                        .then(url => {
+                            var files = [url];
+                            this.setState({
+                                files
+                            });
+                        });
+                }
             });
+
     }
 
     render() {
+        var image = this.state.post && this.state.post.type === "image" && this.state.files ? (
+            <Card.Img src={this.state.files[0]} />
+        ) : null;
         
         var postCard = this.state.post ? (
             <Card className="secondary shadow-sm mt-4 text-left">
                 <Card.Header>
                     <Card.Title className="mb-0">{this.state.post.title}</Card.Title>
                 </Card.Header>
+                { image }
                 <Card.Body>
                     {this.state.post.desc}
                 </Card.Body>
                 <Card.Footer>
-                    Test
+                    by {this.state.username} on {this.state.dateString}
                 </Card.Footer>
             </Card>
         ) : (
@@ -78,7 +109,9 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        getPostByID: (id) => dispatch(getPostByID(id))
+        getPostByID: (id) => dispatch(getPostByID(id)),
+        getUsernameFromUid: (uid) => dispatch(getUsernameFromUid(uid)),
+        getSingleFileURLFromPostId: (postId) => dispatch(getSingleFileURLFromPostId(postId))
     }
 }
 
